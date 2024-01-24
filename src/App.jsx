6cb0e4a847d70c362fc1deceb959de33211525f0
Udeme01 <emmanueluddy2@gmail.com>
2024-01-24 11:41:10 +0100

    tic-tac-toe game

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player1" symbol="X" />
+          <Player initialName="Player2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  // created another state for the input...
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+//   handles players clicks on 'edits' && 'saves'.
+  function handleEdit() {
+    // updates state based on previous state...
+    setIsEditing((prevState) => !prevState);
+  }
+
+//   onChange of playerNames - triggers function below
+function handleChange(e) {
+    setPlayerName(e.target.value);
+}
+
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">
+          {!isEditing ? playerName : <input type="text" required value={playerName} onChange={handleChange} />}
+        </span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
+
+export default Player;
+
+// Added a function that's triggered when the <button> is clicked.
+// Changed isEditing to true in that function.
+// Show the <span className='player-name'> only when isEditing is false.
+// Show an <input> element if isEditing is true.
